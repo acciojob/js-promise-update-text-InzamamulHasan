7d4e1370c0d6848fa2aac8e3f9bde42a38f293ce
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-04-08 21:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let data=document.querySelector("#output");
+let prom=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Hello, world!");
+	})
+})
+prom.then((ans)=>{
+	data.textContent=ans;
+})cath((err)=>{
+	console.log("err",err);
+})
